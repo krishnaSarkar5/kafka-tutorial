5a403dfd3f34a379a9c8ee5f4441adfea25cdad5
krishnasarkar5 <krishnasarkar95@gmail.com>
2023-03-14 06:54:11 +0530

    new topic created

---
+++ kafka-tutorial/src/main/java/com/kafkatutorial/config/KafkaTopicConfig.java
@@ -0,0 +1,19 @@
+package com.kafkatutorial.config;
+
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+
+    @Bean
+    public NewTopic createNewTopic(){
+        return TopicBuilder.name("new_example_topic") // name of the topic
+//                .partitions(10) // no of partition declare at the creation time if not provived it will set a default value
+                .build();
+    }
+}
