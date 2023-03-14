11d1e2849b957a19e10584efe30558ae107184e2
krishnasarkar5 <krishnasarkar95@gmail.com>
2023-03-14 06:42:48 +0530

    first commit

---
+++ kafka-tutorial/src/test/java/com/kafkatutorial/KafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
